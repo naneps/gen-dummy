d7197702869a63317e8e23fdac8d28d81650a084
Nandang Eka Prasetya <72372613+naneps@users.noreply.github.com>
2024-01-29 10:03:28 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   mode: 'development',
   define: {
     'process.env.PROJECT_URL': JSON.stringify(process.env.PROJECT_URL),
-    'process.env.OPEN_AI_KEY.': JSON.stringify(process.env.OPEN_AI_KEY),
+    'process.env.OPEN_AI_KEY': JSON.stringify(process.env.OPEN_AI_KEY),
     'process.env.SUPABASE_KEY': JSON.stringify(process.env.SUPABASE_KEY),
   },
 });
